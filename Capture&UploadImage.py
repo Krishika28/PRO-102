2555989048b7975cca9091b2fdbe9fc6ddb02705
Krishika28 <69198860+Krishika28@users.noreply.github.com>
2021-08-31 20:22:17 +0530

    Update Capture&UploadImage.py

--- Capture&UploadImage.py
+++ Capture&UploadImage.py
@@ -1,16 +1,27 @@
-import cv2
+How to set yourself up for success with a better morning routine
+1. Give yourself more time by waking up earlier
+2. Make your bed
+3. Set your Most Important Tasks for the Day
+4. Connect with your bigger goals by journaling
+5. Meditate to prepare for whatever the day brings
 
-def take_snapshot():
-   
-    videoCaptureObject = cv2.VideoCapture(0)
-    result = True
-    while(result):
-      
-        ret,frame = videoCaptureObject.read()
-        cv2.imwrite("NewPicture1.jpg",frame)
-        result = False
+The essential workday habits that keep you focused and productive
+6. Skip email first thing in the morning
+7. Eat the frog (tackle something difficult when your energy is highest)
+8. Schedule (and take) more breaks
+9. "Batch" similar work together
+10. Set hard limits on certain activities
+11. Schedule your email and IM time (or create "office hours")
+12. Use GTD to build a habit of staying organized
 
-    videoCaptureObject.release()
-    cv2.destroyAllWindows()
+Disconnecting from work
+13. Reflect on your accomplishments and write down 3 good things that happened
+14. Make space for mental solitude
+15. Spend time on a hobby
+16. Prepare for tomorrow with a ‘shutdown ritual’
+17. Turn off your devices at least 30 minutes before bed
 
-take_snapshot()
+Habits that optimize for energy and health
+19. Give your eyes a break
+20. Drink more water during the day
+21. Regular exercise
