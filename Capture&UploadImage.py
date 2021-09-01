ae2156f13236920ad305913e912375c6e9f51ab1
Krishika28 <69198860+Krishika28@users.noreply.github.com>
2021-09-01 12:36:35 +0530

    Update Capture&UploadImage.py

--- Capture&UploadImage.py
+++ Capture&UploadImage.py
@@ -1,13 +1,13 @@
-                                                                Do These Things To Keep Yourself Heath
+                                                                ※Do These Things To Keep Yourself Heath※
 
-How to set yourself up for success with a better morning routine
+                                                  ※How to set yourself up for success with a better morning routine※
 1. Give yourself more time by waking up earlier
 2. Make your bed
 3. Set your Most Important Tasks for the Day
 4. Connect with your bigger goals by journaling
 5. Meditate to prepare for whatever the day brings
 
-The essential workday habits that keep you focused and productive
+                                                  ※The essential workday habits that keep you focused and productive※
 6. Skip email first thing in the morning
 7. Eat the frog (tackle something difficult when your energy is highest)
 8. Schedule (and take) more breaks
@@ -16,14 +16,14 @@ The essential workday habits that keep you focused and productive
 11. Schedule your email and IM time (or create "office hours")
 12. Use GTD to build a habit of staying organized
 
-Disconnecting from work
+                                                                           ※Disconnecting from work※
 13. Reflect on your accomplishments and write down 3 good things that happened
 14. Make space for mental solitude
 15. Spend time on a hobby
 16. Prepare for tomorrow with a ‘shutdown ritual’
 17. Turn off your devices at least 30 minutes before bed
 
-Habits that optimize for energy and health
+                                                                  ※Habits that optimize for energy and health※
 19. Give your eyes a break
 20. Drink more water during the day
 21. Regular exercise
